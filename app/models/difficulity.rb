2622f7200fd61bab8b3bbb15c200ee96785a86f9
magatamaamagata <magatamaamagata@gmail.com>
2020-10-24 15:36:06 +0900

    Make active_hash model

--- app/models/difficulity.rb
+++ app/models/difficulity.rb
@@ -1,2 +1,9 @@
 class Difficulity < ApplicationRecord
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '練習試合' },
+    { id: 3, name: '市民大会' },
+    { id: 4, name: '県大会' },
+    { id: 5, name: '全国大会' },
+  ]
 end
--- app/models/judge_correct.rb
+++ app/models/judge_correct.rb
@@ -1,2 +1,7 @@
 class JudgeCorrect < ApplicationRecord
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '一本' },
+    { id: 3, name: '無効' },
+  ]
 end
