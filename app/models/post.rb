b38c5c83274b1f5518707c529b973502201459a8
magatamaamagata <magatamaamagata@gmail.com>
2020-10-24 15:43:45 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :judge_correct, :difficulity
+  validates :judge_correct, :difficulity, presence: true
+  validates :judge_correct_id, :difficulity_id, numericality: { other_than: 1 } 
 end
