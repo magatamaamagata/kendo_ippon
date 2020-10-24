f570c2c6daf7d698e9ba6fae203da5853db99e98
magatamaamagata <magatamaamagata@gmail.com>
2020-10-24 23:27:44 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :destroy ,:update]
+  before_action :authenticate_user!, only: [:new,:edit,:update]
+  before_action :move_to_index, only: [:edit, :update, :delete]
   def index
   end
   def new
@@ -13,8 +16,41 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+  def move_to_index
+    unless user_signed_in? && (current_user.id == @post.user_id)
+      redirect_to root_path 
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:judge_correct_id, :difficulity_id).merge(user_id: current_user.id)
+  end
+
 end
 
-def post_params
-  params.require(:post).permit(:judge_correct_id, :difficulity_id).merge(user_id: current_user.id)
-end
