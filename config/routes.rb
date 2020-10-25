f834f1d0495331f3249e79ae7ad554b1c0189abf
magatamaamagata <magatamaamagata@gmail.com>
2020-10-25 23:27:23 +0900

    Rueting nest

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'judges/show'
   root to: 'posts#index'
   devise_for :users
-  resources :posts
-  resources :judges, only: [:create,:show]
+  resources :posts do
+    resources :judges, only: [:create,:index]
+  end
 end
