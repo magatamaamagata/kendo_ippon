279419db5737120fba1f11d18bca8842b7d00f3c
magatamaamagata <magatamaamagata@gmail.com>
2020-10-25 23:30:38 +0900

    Update judges_controller.rb

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -1,16 +1,17 @@
 class JudgesController < ApplicationController
-  def create
-    @judge = Judge.new(judge_params)
-    @judge.save
-  end
-
-  def show
-    @judge = Judge.find(params[:id])
+  def index
+    @judge = Judge.find(params[:post_id])
     @judges = Judge.includes(:post).order('created_at DESC')
+  end
+  def create
+    judge = Judge.new(judge_params)
+    judge.save
+    redirect_to  action: :index
   end
 
 private
   def judge_params
-    params.require(:judge,:comment).permit(:judge)
+    @post = Post.find(params[:post_id])
+    params.require(:judge).permit(:comment,:judge_correct_id).merge(user_id: current_user.id, post_id: @post.id)
   end
 end
