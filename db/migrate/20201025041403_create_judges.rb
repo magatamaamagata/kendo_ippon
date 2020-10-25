88ba6aaedd16b03df619b79ee9a4663771f7fb3b
magatamaamagata <magatamaamagata@gmail.com>
2020-10-25 23:27:05 +0900

    Update judges migration file

--- db/migrate/20201025041403_create_judges.rb
+++ db/migrate/20201025041403_create_judges.rb
@@ -1,7 +1,10 @@
 class CreateJudges < ActiveRecord::Migration[6.0]
   def change
     create_table :judges do |t|
-
+      t.integer :judge_correct_id,   null: false
+      t.text :comment
+      t.references :user,   null: false, foreign_key: true
+      t.references :post,   null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_054755) do
+ActiveRecord::Schema.define(version: 2020_10_25_041403) do
+
+  create_table "judges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "judge_correct_id", null: false
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_judges_on_post_id"
+    t.index ["user_id"], name: "index_judges_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "gif_url"
@@ -38,5 +49,7 @@ ActiveRecord::Schema.define(version: 2020_10_24_054755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "judges", "posts"
+  add_foreign_key "judges", "users"
   add_foreign_key "posts", "users"
 end
