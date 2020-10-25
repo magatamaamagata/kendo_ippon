09ebc362d40d31215f6b5ef51c829142c98a3df2
magatamaamagata <magatamaamagata@gmail.com>
2020-10-25 23:30:33 +0900

    Update judge.rb

--- app/models/judge.rb
+++ app/models/judge.rb
@@ -1,4 +1,10 @@
 class Judge < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :judge_correct 
+  
   belongs_to :user
   belongs_to :post
+  validates  :comment, presence: true
+  validates :judge_correct_id, numericality: { other_than: 1 } 
+
 end
