80bc276037821fa6ba37f67e0eb9b7ef01116450
magatamaamagata <magatamaamagata@gmail.com>
2020-10-28 15:40:53 +0900

    Inastall capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  server '18.178.123.245', user: 'ec2-user', roles: %w{app db web}
+
 end
