b190e6fa541342517cb9f4eada366430f3a0fd1d
magatamaamagata <magatamaamagata@gmail.com>
2020-10-29 23:57:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :judges, only: [:create,:index]
   end
+  resources :skills
+  resources :compares, only: [:create,:index] do
+    resources :comments
+  end
 end
