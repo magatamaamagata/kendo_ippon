9c037f4be5641bf8c706dd77aeee4e6ebbf0b627
magatamaamagata <magatamaamagata@gmail.com>
2020-11-01 22:52:05 +0900

    Update skill.rb

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,8 +3,8 @@ class Skill < ApplicationRecord
   # belongs_to_active_hash :public
   has_one_attached :video
   belongs_to :user
-  has_one :compare
-  has_many :comments
+  has_one :compare, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   # validates :public, presence: true
   # validates :public_id, numericality: { other_than: 1 } 
