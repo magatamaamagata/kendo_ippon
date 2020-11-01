2cc23f19778e723e6ae2db888169c2ea5a4bdd8a
magatamaamagata <magatamaamagata@gmail.com>
2020-11-01 22:39:05 +0900

    Add comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,12 @@ class CommentsController < ApplicationController
   def create
     @comments = Comment.all
     @comment = Comment.create(comment_params)
+    binding.pry
     render json:{ comment: @comment }
   end
 
   private
   def comment_params
-    params.permit(:authenticity_token, :commit,:skill_id, :compare_id, compare: [:comment]).merge(user_id: current_user.id)
+    params.require(:comment).permit(:text, compare: [:comment]).merge(user_id: current_user.id)
   end
 end
