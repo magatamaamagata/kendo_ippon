f50e05627f8da129feb0f66656c843e2032cea9a
magatamaamagata <magatamaamagata@gmail.com>
2020-11-01 09:00:29 +0900

    Delete compare assosiation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :skill
-  belongs_to :compare
 end
--- app/models/compare.rb
+++ app/models/compare.rb
@@ -1,5 +1,3 @@
 class Compare < ApplicationRecord
   belongs_to :skill
-  has_one_attached :video
-  has_many :comments
 end
