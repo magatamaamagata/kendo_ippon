ea9c692e49f8f32068da298c30c419ed757aa6d0
magatamaamagata <magatamaamagata@gmail.com>
2020-11-04 15:19:05 +0900

    Add video render file

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,10 +1,10 @@
 class Youtube < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '面', url_id: "1"},
-    { id: 3, name: '小手', url_id: "2"},
-    { id: 4, name: '胴', url_id: "3"},
-    { id: 5, name: '突き', url_id: "4"},
+    { id: 2, name: '面', url_id: "SlctMcLHP8U"},
+    { id: 3, name: '小手', url_id: "Tlg_ngHsXoI"},
+    { id: 4, name: '胴', url_id: "A1hUUwp4Kkw"},
+    { id: 5, name: '突き', url_id: "Zl-PHUCkNZA"},
   ]
 end
 
