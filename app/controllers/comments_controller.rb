8bbd98806dbf29769511970207bc97cc569d0a50
magatamaamagata <magatamaamagata@gmail.com>
2020-11-05 12:33:18 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :move_to_index
+  before_action :authenticate_user!
+
   def new
     @comment = Comment.new
     @skill = Skill.find(params[:skill_id])
@@ -10,6 +13,10 @@ class CommentsController < ApplicationController
   end
 
   private
+  def move_to_index
+    @comment = Comment.find(params[:id])
+    redirect_to root_path unless user_signed_in? && (current_user.id == @skill.user.id)
+  end
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, skill_id: params[:skill_id])
