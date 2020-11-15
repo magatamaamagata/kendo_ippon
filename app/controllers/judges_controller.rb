d35e49dd9c3d5aa8aefe33d9e35e8b1ae721074d
magatamaamagata <magatamaamagata@gmail.com>
2020-11-15 17:07:04 +0900

    Fix judge function

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -1,19 +1,24 @@
 class JudgesController < ApplicationController
   def index
-    @judge = Judge.find(params[:post_id])
+    # @post=Post.find(params[:post_id])
+    # @judge = Judge.find(@post.judge.id)
     @judges = Judge.includes(:post).order('created_at DESC')
   end
 
   def create
-    judge = Judge.new(judge_params)
-    judge.save
-    redirect_to action: :index
+    @judge = Judge.new(judge_params)
+    if @judge.save && (@judge.post.judge_correct_id == @judge.judge_correct_id)
+      redirect_to post_judges_path
+    else
+      # flash[:ng] = "外れです"
+      redirect_to posts_path
+    end
   end
 
   private
 
   def judge_params
     @post = Post.find(params[:post_id])
-    params.require(:judge).permit(:comment, :judge_correct_id).merge(user_id: current_user.id, post_id: @post.id)
+    params.require(:judge).permit(:judge_correct_id).merge(user_id: current_user.id, post_id: @post.id)
   end
 end
--- app/models/judge.rb
+++ app/models/judge.rb
@@ -4,6 +4,5 @@ class Judge < ApplicationRecord
 
   belongs_to :user
   belongs_to :post
-  validates  :comment, presence: true
   validates :judge_correct_id, numericality: { other_than: 1 }
 end
