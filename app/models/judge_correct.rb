6abeaebce0f9d22a1d7df3c74bba943398f46201
magatamaamagata <magatamaamagata@gmail.com>
2020-11-17 17:25:58 +0900

    Update view

--- app/models/judge_correct.rb
+++ app/models/judge_correct.rb
@@ -1,7 +1,7 @@
 class JudgeCorrect < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '一本' },
-    { id: 3, name: '無効' }
+    { id: 2, name: '一本'},
+    { id: 3, name: '無効'}
   ]
 end
