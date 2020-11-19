5418ddf41f903a29aaef656a46f7c8c969e78554
magatamaamagata <magatamaamagata@gmail.com>
2020-11-19 10:09:58 +0900

    Make users integration test

--- spec/factories/compares.rb
+++ spec/factories/compares.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     make_better { Faker::Lorem.characters(number: 400) }
     notice { Faker::Lorem.characters(number: 400) }
     public_id {Faker::Number.between(from: 2, to: 3)}
+    association :skill
   end
 end
