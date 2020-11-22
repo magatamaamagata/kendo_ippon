bc9d427fd50e7e2f0c6b7984b23fff784d7fa31b
magatamaamagata <magatamaamagata@gmail.com>
2020-11-22 18:22:35 +0900

    Update youtube.rb

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -2,8 +2,8 @@ class Youtube < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '面', url_id: 'SlctMcLHP8U',start: "0" },
-    { id: 3, name: '小手', url_id: 'V6rqqI74q-0',start: "7400" },
+    { id: 3, name: '小手', url_id: 'V6rqqI74q-0',start: "74" },
     { id: 4, name: '胴', url_id: 'kxceVkCnNdI',start: "325"},
-    { id: 5, name: '突き', url_id: 'eEG6z-5ycpQ',start: "1895000" }
+    { id: 5, name: '突き', url_id: 'eEG6z-5ycpQ',start: "1895" }
   ]
 end
